37a8e1ea0e80dfae6e802bff0efaf03b3b68c5a2
pipimiao <1310275859@qq.com>
2024-11-24 23:10:25 +0800

    小测试

--- function_1.py
+++ function_1.py
@@ -5,3 +5,4 @@ pic_file_lis = glob.glob(r'pic\*.jpg')
 target_file = r'F:\deeplearning\pytorch\miao_tools\miaomiao_cat'
 for file in pic_file_lis:
     shutil.move(file,target_file)
+    print('完成传送')
